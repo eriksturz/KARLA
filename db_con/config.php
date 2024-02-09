a318e00ffc5b663641880fb6caa576fc297ea789
eriksturz <131181319+eriksturz@users.noreply.github.com>
2024-02-09 09:42:55 +0100

    Delete db_con/config.php

--- db_con/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = 'localhost'; // Hostname des MySQL-Servers
-$user = 'root'; // Benutzername für die Datenbank
-$pass = 'test'; // Passwort für die Datenbank
-$dbname = 'karla'; // Name der Datenbank
-?>
